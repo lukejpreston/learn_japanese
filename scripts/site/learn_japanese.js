bb0f96b84eb0aa057964513bce0ed36e3f2bf057
Luke Preston <luke.preston@bbc.co.uk>
2016-06-03 08:09:55 +0100

    reveal is nicer

--- scripts/site/learn_japanese.js
+++ scripts/site/learn_japanese.js
@@ -1,180 +1,36 @@
-window.alphabet = [
-  {type: 'HIRAGANA', row: ' ', col: 'A', code: '&#12354;'},
-  {type: 'HIRAGANA', row: ' ', col: 'I', code: '&#12356;'},
-  {type: 'HIRAGANA', row: ' ', col: 'U', code: '&#12358;'},
-  {type: 'HIRAGANA', row: ' ', col: 'E', code: '&#12360;'},
-  {type: 'HIRAGANA', row: ' ', col: 'O', code: '&#12362;'},
-  {type: 'HIRAGANA', row: 'K', col: 'A', code: '&#12363;'},
-  {type: 'HIRAGANA', row: 'G', col: 'A', code: '&#12364;'},
-  {type: 'HIRAGANA', row: 'K', col: 'I', code: '&#12365;'},
-  {type: 'HIRAGANA', row: 'G', col: 'I', code: '&#12366;'},
-  {type: 'HIRAGANA', row: 'K', col: 'U', code: '&#12367;'},
-  {type: 'HIRAGANA', row: 'G', col: 'U', code: '&#12368;'},
-  {type: 'HIRAGANA', row: 'K', col: 'E', code: '&#12369;'},
-  {type: 'HIRAGANA', row: 'G', col: 'E', code: '&#12370;'},
-  {type: 'HIRAGANA', row: 'K', col: 'O', code: '&#12371;'},
-  {type: 'HIRAGANA', row: 'G', col: 'O', code: '&#12372;'},
-  {type: 'HIRAGANA', row: 'S', col: 'A', code: '&#12373;'},
-  {type: 'HIRAGANA', row: 'Z', col: 'A', code: '&#12374;'},
-  {type: 'HIRAGANA', row: 'S', col: 'I', code: '&#12375;'},
-  {type: 'HIRAGANA', row: 'Z', col: 'I', code: '&#12376;'},
-  {type: 'HIRAGANA', row: 'S', col: 'U', code: '&#12377;'},
-  {type: 'HIRAGANA', row: 'Z', col: 'U', code: '&#12378;'},
-  {type: 'HIRAGANA', row: 'S', col: 'E', code: '&#12379;'},
-  {type: 'HIRAGANA', row: 'Z', col: 'E', code: '&#12380;'},
-  {type: 'HIRAGANA', row: 'S', col: 'O', code: '&#12381;'},
-  {type: 'HIRAGANA', row: 'Z', col: 'O', code: '&#12382;'},
-  {type: 'HIRAGANA', row: 'T', col: 'A', code: '&#12383;'},
-  {type: 'HIRAGANA', row: 'D', col: 'A', code: '&#12384;'},
-  {type: 'HIRAGANA', row: 'T', col: 'I', code: '&#12385;'},
-  {type: 'HIRAGANA', row: 'D', col: 'I', code: '&#12386;'},
-  {type: 'HIRAGANA', row: 'T', col: 'U', code: '&#12388;'},
-  {type: 'HIRAGANA', row: 'D', col: 'U', code: '&#12389;'},
-  {type: 'HIRAGANA', row: 'T', col: 'E', code: '&#12390;'},
-  {type: 'HIRAGANA', row: 'D', col: 'E', code: '&#12391;'},
-  {type: 'HIRAGANA', row: 'T', col: 'O', code: '&#12392;'},
-  {type: 'HIRAGANA', row: 'D', col: 'O', code: '&#12393;'},
-  {type: 'HIRAGANA', row: 'N', col: 'A', code: '&#12394;'},
-  {type: 'HIRAGANA', row: 'N', col: 'I', code: '&#12395;'},
-  {type: 'HIRAGANA', row: 'N', col: 'U', code: '&#12396;'},
-  {type: 'HIRAGANA', row: 'N', col: 'E', code: '&#12397;'},
-  {type: 'HIRAGANA', row: 'N', col: 'O', code: '&#12398;'},
-  {type: 'HIRAGANA', row: 'H', col: 'A', code: '&#12399;'},
-  {type: 'HIRAGANA', row: 'B', col: 'A', code: '&#12400;'},
-  {type: 'HIRAGANA', row: 'P', col: 'A', code: '&#12401;'},
-  {type: 'HIRAGANA', row: 'H', col: 'I', code: '&#12402;'},
-  {type: 'HIRAGANA', row: 'B', col: 'I', code: '&#12403;'},
-  {type: 'HIRAGANA', row: 'P', col: 'I', code: '&#12404;'},
-  {type: 'HIRAGANA', row: 'H', col: 'U', code: '&#12405;'},
-  {type: 'HIRAGANA', row: 'B', col: 'U', code: '&#12406;'},
-  {type: 'HIRAGANA', row: 'P', col: 'U', code: '&#12407;'},
-  {type: 'HIRAGANA', row: 'H', col: 'E', code: '&#12408;'},
-  {type: 'HIRAGANA', row: 'B', col: 'E', code: '&#12409;'},
-  {type: 'HIRAGANA', row: 'P', col: 'E', code: '&#12410;'},
-  {type: 'HIRAGANA', row: 'H', col: 'O', code: '&#12411;'},
-  {type: 'HIRAGANA', row: 'B', col: 'O', code: '&#12412;'},
-  {type: 'HIRAGANA', row: 'P', col: 'O', code: '&#12413;'},
-  {type: 'HIRAGANA', row: 'M', col: 'A', code: '&#12414;'},
-  {type: 'HIRAGANA', row: 'M', col: 'I', code: '&#12415;'},
-  {type: 'HIRAGANA', row: 'M', col: 'U', code: '&#12416;'},
-  {type: 'HIRAGANA', row: 'M', col: 'E', code: '&#12417;'},
-  {type: 'HIRAGANA', row: 'M', col: 'O', code: '&#12418;'},
-  {type: 'HIRAGANA', row: 'Y', col: 'A', code: '&#12420;'},
-  {type: 'HIRAGANA', row: 'Y', col: 'U', code: '&#12422;'},
-  {type: 'HIRAGANA', row: 'Y', col: 'O', code: '&#12424;'},
-  {type: 'HIRAGANA', row: 'R', col: 'A', code: '&#12425;'},
-  {type: 'HIRAGANA', row: 'R', col: 'I', code: '&#12426;'},
-  {type: 'HIRAGANA', row: 'R', col: 'U', code: '&#12427;'},
-  {type: 'HIRAGANA', row: 'R', col: 'E', code: '&#12428;'},
-  {type: 'HIRAGANA', row: 'R', col: 'O', code: '&#12429;'},
-  {type: 'HIRAGANA', row: 'W', col: 'A', code: '&#12431;'},
-  {type: 'HIRAGANA', row: 'W', col: 'I', code: '&#12432;'},
-  {type: 'HIRAGANA', row: 'W', col: 'E', code: '&#12433;'},
-  {type: 'HIRAGANA', row: 'W', col: 'O', code: '&#12434;'},
-  {type: 'HIRAGANA', row: 'N', col: ' ', code: '&#12435;'},
-  {type: 'HIRAGANA', row: 'V', col: 'U', code: '&#12436;'},
-  {type: 'KATAKANA', row: ' ', col: 'A', code: '&#12450;'},
-  {type: 'KATAKANA', row: ' ', col: 'I', code: '&#12452;'},
-  {type: 'KATAKANA', row: ' ', col: 'U', code: '&#12454;'},
-  {type: 'KATAKANA', row: ' ', col: 'E', code: '&#12456;'},
-  {type: 'KATAKANA', row: ' ', col: 'O', code: '&#12458;'},
-  {type: 'KATAKANA', row: 'K', col: 'A', code: '&#12459;'},
-  {type: 'KATAKANA', row: 'G', col: 'A', code: '&#12460;'},
-  {type: 'KATAKANA', row: 'K', col: 'I', code: '&#12461;'},
-  {type: 'KATAKANA', row: 'G', col: 'I', code: '&#12462;'},
-  {type: 'KATAKANA', row: 'K', col: 'U', code: '&#12463;'},
-  {type: 'KATAKANA', row: 'G', col: 'U', code: '&#12464;'},
-  {type: 'KATAKANA', row: 'K', col: 'E', code: '&#12465;'},
-  {type: 'KATAKANA', row: 'G', col: 'E', code: '&#12466;'},
-  {type: 'KATAKANA', row: 'K', col: 'O', code: '&#12467;'},
-  {type: 'KATAKANA', row: 'G', col: 'O', code: '&#12468;'},
-  {type: 'KATAKANA', row: 'S', col: 'A', code: '&#12469;'},
-  {type: 'KATAKANA', row: 'Z', col: 'A', code: '&#12470;'},
-  {type: 'KATAKANA', row: 'S', col: 'I', code: '&#12471;'},
-  {type: 'KATAKANA', row: 'Z', col: 'I', code: '&#12472;'},
-  {type: 'KATAKANA', row: 'S', col: 'U', code: '&#12473;'},
-  {type: 'KATAKANA', row: 'Z', col: 'U', code: '&#12474;'},
-  {type: 'KATAKANA', row: 'S', col: 'E', code: '&#12475;'},
-  {type: 'KATAKANA', row: 'Z', col: 'E', code: '&#12476;'},
-  {type: 'KATAKANA', row: 'S', col: 'O', code: '&#12477;'},
-  {type: 'KATAKANA', row: 'Z', col: 'O', code: '&#12478;'},
-  {type: 'KATAKANA', row: 'T', col: 'A', code: '&#12479;'},
-  {type: 'KATAKANA', row: 'D', col: 'A', code: '&#12480;'},
-  {type: 'KATAKANA', row: 'T', col: 'I', code: '&#12481;'},
-  {type: 'KATAKANA', row: 'D', col: 'I', code: '&#12482;'},
-  {type: 'KATAKANA', row: 'T', col: 'U', code: '&#12484;'},
-  {type: 'KATAKANA', row: 'D', col: 'U', code: '&#12485;'},
-  {type: 'KATAKANA', row: 'T', col: 'E', code: '&#12486;'},
-  {type: 'KATAKANA', row: 'D', col: 'E', code: '&#12487;'},
-  {type: 'KATAKANA', row: 'T', col: 'O', code: '&#12488;'},
-  {type: 'KATAKANA', row: 'D', col: 'O', code: '&#12489;'},
-  {type: 'KATAKANA', row: 'N', col: 'A', code: '&#12490;'},
-  {type: 'KATAKANA', row: 'N', col: 'I', code: '&#12491;'},
-  {type: 'KATAKANA', row: 'N', col: 'U', code: '&#12492;'},
-  {type: 'KATAKANA', row: 'N', col: 'E', code: '&#12493;'},
-  {type: 'KATAKANA', row: 'N', col: 'O', code: '&#12494;'},
-  {type: 'KATAKANA', row: 'H', col: 'A', code: '&#12495;'},
-  {type: 'KATAKANA', row: 'B', col: 'A', code: '&#12496;'},
-  {type: 'KATAKANA', row: 'P', col: 'A', code: '&#12497;'},
-  {type: 'KATAKANA', row: 'H', col: 'I', code: '&#12498;'},
-  {type: 'KATAKANA', row: 'B', col: 'I', code: '&#12499;'},
-  {type: 'KATAKANA', row: 'P', col: 'I', code: '&#12500;'},
-  {type: 'KATAKANA', row: 'H', col: 'U', code: '&#12501;'},
-  {type: 'KATAKANA', row: 'B', col: 'U', code: '&#12502;'},
-  {type: 'KATAKANA', row: 'P', col: 'U', code: '&#12503;'},
-  {type: 'KATAKANA', row: 'H', col: 'E', code: '&#12504;'},
-  {type: 'KATAKANA', row: 'B', col: 'E', code: '&#12505;'},
-  {type: 'KATAKANA', row: 'P', col: 'E', code: '&#12506;'},
-  {type: 'KATAKANA', row: 'H', col: 'O', code: '&#12507;'},
-  {type: 'KATAKANA', row: 'B', col: 'O', code: '&#12508;'},
-  {type: 'KATAKANA', row: 'P', col: 'O', code: '&#12509;'},
-  {type: 'KATAKANA', row: 'M', col: 'A', code: '&#12510;'},
-  {type: 'KATAKANA', row: 'M', col: 'I', code: '&#12511;'},
-  {type: 'KATAKANA', row: 'M', col: 'U', code: '&#12512;'},
-  {type: 'KATAKANA', row: 'M', col: 'E', code: '&#12513;'},
-  {type: 'KATAKANA', row: 'M', col: 'O', code: '&#12514;'},
-  {type: 'KATAKANA', row: 'Y', col: 'A', code: '&#12516;'},
-  {type: 'KATAKANA', row: 'Y', col: 'U', code: '&#12518;'},
-  {type: 'KATAKANA', row: 'Y', col: 'O', code: '&#12520;'},
-  {type: 'KATAKANA', row: 'R', col: 'A', code: '&#12521;'},
-  {type: 'KATAKANA', row: 'R', col: 'I', code: '&#12522;'},
-  {type: 'KATAKANA', row: 'R', col: 'U', code: '&#12523;'},
-  {type: 'KATAKANA', row: 'R', col: 'E', code: '&#12524;'},
-  {type: 'KATAKANA', row: 'R', col: 'O', code: '&#12525;'},
-  {type: 'KATAKANA', row: 'W', col: 'A', code: '&#12527;'},
-  {type: 'KATAKANA', row: 'W', col: 'I', code: '&#12528;'},
-  {type: 'KATAKANA', row: 'W', col: 'E', code: '&#12529;'},
-  {type: 'KATAKANA', row: 'W', col: 'O', code: '&#12530;'},
-  {type: 'KATAKANA', row: 'N', col: ' ', code: '&#12531;'},
-  {type: 'KATAKANA', row: 'V', col: 'U', code: '&#12532;'},
-  {type: 'KATAKANA', row: 'V', col: 'A', code: '&#12535;'},
-  {type: 'KATAKANA', row: 'V', col: 'I', code: '&#12536;'},
-  {type: 'KATAKANA', row: 'V', col: 'E', code: '&#12537;'},
-  {type: 'KATAKANA', row: 'V', col: 'O', code: '&#12538;'}
-]
-
 function getLetter() {
-  var choice = Math.floor(Math.random() * alphabet.length)
-  var letter = alphabet[choice]
+  var letters = []
+  var hiragana = dom.query('#hiragana').checked
+  var katakana = dom.query('#katakana').checked
+  var grade_1 = dom.query('#grade_1').checked
+  var grade_2 = dom.query('#grade_2').checked
+  var grade_3 = dom.query('#grade_3').checked
+  var grade_4 = dom.query('#grade_4').checked
+  var grade_5 = dom.query('#grade_5').checked
+  var grade_6 = dom.query('#grade_6').checked
 
-  var shouldBeKatakana = dom.query('#katakana').checked
-  var shouldBeHiragana = dom.query('#hiragana').checked
+  if(hiragana) letters = letters.concat(kana.hiragana)
+  if(katakana) letters = letters.concat(kana.katakana)
+  if(grade_1) letters = letters.concat(kanji[0])
+  if(grade_2) letters = letters.concat(kanji[1])
+  if(grade_3) letters = letters.concat(kanji[2])
+  if(grade_4) letters = letters.concat(kanji[3])
+  if(grade_5) letters = letters.concat(kanji[4])
+  if(grade_6) letters = letters.concat(kanji[5])
 
-  if(!shouldBeKatakana && !shouldBeHiragana) return letter
-  else if(!shouldBeKatakana && letter.type === 'KATAKANA') return getLetter()
-  else if(!shouldBeHiragana && letter.type === 'HIRAGANA') return getLetter()
-  else return letter
+  var number = Math.floor(Math.random() * letters.length)
+  return letters[number]
 }
 
 function generate() {
   window.letter = getLetter()
   dom.query('#character').innerHTML = window.letter.code
 
-  var row = window.letter.row
-  if(window.letter.row === ' ') row = ''
-  var col = window.letter.col
-  if(window.letter.col === ' ') col = ''
-  window.answer = row + col
+  if(window.letter.hasOwnProperty('translation')) {
+    dom.query('#kanji_translation').innerHTML = window.letter.translation
+    dom.query('#kanji_romanji').innerHTML = window.letter.romanji.join(', ')
+  } else
+    dom.query('#kana_romanji').innerHTML = window.letter.romanji
 
   dom.query('#mark')
     .changeClass('icon-checkmark', 'icon-cross')
@@ -186,38 +42,24 @@ function generate() {
 }
 
 function validate(word) {
-  word = word.toUpperCase()
-  if(word === window.answer) {
-    dom.query('#mark')
-      .changeClass('icon-cross', 'icon-checkmark')
-      .changeClass('button-incorrect', 'button-correct')
-    reveal()
-    setTimeout(function() {
-      generate()
-    }, 1000)
-  }
+
 }
 
 function reveal() {
-  dom.query('#reveal').innerHTML = window.answer.toLowerCase()
-  setTimeout(function() {
-    dom.query('#reveal').innerHTML = ''
+  var revealChar = dom.query('#kana_reveal')
+  if(window.letter.hasOwnProperty('translation')) revealChar = dom.query('#kanji_reveal')
+  revealChar.stylise({
+    display: 'inline-block'
+  })
+  setTimeout(function () {
+    revealChar.stylise({
+      display: 'none'
+    })
   }, 1000)
-  dom.query('#input').focus()
 }
 
 function say() {
-  var vowelSounds = {
-    A: 'ahh',
-    E: 'eh',
-    I: 'e',
-    O: 'oh',
-    u: 'uh',
-    ' ': ''
-  }
 
-  var sound = window.letter.row.toLowerCase() + vowelSounds[window.letter.col]
-  responsiveVoice.speak(sound, 'Japanese Female')
 }
 
 generate()
