3059f7149048aeead66c722e6c729bb3c472b731
Luke Preston <luke.preston@bbc.co.uk>
2016-06-03 08:39:01 +0100

    game is correct

--- scripts/site/learn_japanese.js
+++ scripts/site/learn_japanese.js
@@ -27,36 +27,79 @@ function generate() {
   dom.query('#character').innerHTML = window.letter.code
 
   if(window.letter.hasOwnProperty('translation')) {
+    window.noTranslation = false
     dom.query('#kanji_translation').innerHTML = window.letter.translation
     dom.query('#kanji_romanji').innerHTML = window.letter.romanji.join(', ')
     dom.query('#translation').stylise({
       display: 'block'
     })
   } else {
+    window.noTranslation = true
     dom.query('#kana_romanji').innerHTML = window.letter.romanji
     dom.query('#translation').stylise({
       display: 'none'
     })
   }
 
-  dom.query('#mark')
+  dom.query('#romanji_mark')
     .changeClass('icon-checkmark', 'icon-cross')
     .changeClass('button-correct', 'button-incorrect')
+  dom.query('#translation_mark')
+    .changeClass('icon-checkmark', 'icon-cross')
+    .changeClass('button-correct', 'button-incorrect')
+  var romanjiInput = dom.query('#romanji_input')
+  romanjiInput.value = ''
+  var translationInput = dom.query('#translation_input')
+  translationInput.value = ''
+  romanjiInput.focus()
+}
 
-  var input = dom.query('#input')
-  input.value = ''
-  input.focus()
+function success() {
+  if(window.romanjiCorrect && (window.translationCorrect || window.noTranslation))
+  reveal(function() {
+    window.romanjiCorrect = false
+    window.translationCorrect = false
+    window.noTranslation = false
+    generate()
+  })
 }
 
 function validateRomanji(word) {
-
+  word = word.toLowerCase()
+  var romanji = [].concat(window.letter.romanji)
+  if(romanji.indexOf(word) !== -1) {
+    window.romanjiCorrect = true
+    dom.query('#romanji_mark')
+      .changeClass('icon-cross', 'icon-checkmark')
+      .changeClass('button-incorrect', 'button-correct')
+    success()
+  } else {
+    window.romanjiCorrect = false
+    dom.query('#romanji_mark')
+      .changeClass('icon-checkmark', 'icon-cross')
+      .changeClass('button-correct', 'button-incorrect')
+  }
 }
 
 function validateTranslation(word) {
-
+  word = word.toLowerCase()
+  var translation = [].concat(window.letter.translation)
+  if(translation.indexOf(word) !== -1) {
+    window.translationCorrect = true
+    dom.query('#translation_mark')
+      .changeClass('icon-cross', 'icon-checkmark')
+      .changeClass('button-incorrect', 'button-correct')
+    success()
+  } else {
+    window.translationCorrect = false
+    dom.query('#translation_mark')
+      .changeClass('icon-checkmark', 'icon-cross')
+      .changeClass('button-correct', 'button-incorrect')
+  }
 }
 
-function reveal() {
+function reveal(callback) {
+  callback = callback || function() {}
   var revealChar = dom.query('#kana_reveal')
   if(window.letter.hasOwnProperty('translation')) revealChar = dom.query('#kanji_reveal')
   revealChar.stylise({
@@ -66,6 +109,7 @@ function reveal() {
     revealChar.stylise({
       display: 'none'
     })
+    callback()
   }, 1000)
 }
 
